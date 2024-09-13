d8fd76bcefd10fc9ac3800a853ed62a3e60cc5ca
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-09-13 20:18:01 -0300

    middleware validacion de usuario

---
+++ middlewares/auth.js
@@ -0,0 +1,21 @@
+import jwt, { decode } from 'jsonwebtoken'
+
+const { JWT_SECRET } = process.env
+
+export default function authUser (req, res, next) {
+    const authorization = req.get('authorization')
+    let token = ''
+
+    if (authorization && authorization.toLowerCase().startsWith('bearer')) {
+        token = authorization.substring(7)
+    }
+    const decodedToken = jwt.verify(token, JWT_SECRET)
+
+    if (!token || !decodedToken.userId) {
+        return res.status(401).json({ error: 'token missing or invalid.'})
+    }
+
+    const { userId } = decodedToken
+    req.userId = userId
+    next()
+}
