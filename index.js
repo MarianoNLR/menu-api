e3bd6d98afc021665573a952eae5a46375f96529
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-09-13 20:18:30 -0300

    + uso de middlewares basicos

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import express from 'express'
 import 'dotenv/config'
+import bodyParser from 'body-parser'
+import cors from 'cors'
 const app = express()
 
 const PORT = process.env.PORT
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors({
+    origin: '*',
+    credentials: true
+}))
+
 
 app.get('/', (req, res) => {
     res.send(`<h1>Hola Mundo</h1>`)
