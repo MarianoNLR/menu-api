2adca1d31352fa1d5f17858206f990dc88c1e414
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-09-15 21:13:13 -0300

    + restaurant controller

---
+++ controllers/restaurantController.js
@@ -0,0 +1,69 @@
+import Restaurant from "../models/restaurantModel.js";
+
+export async function getAll (req, res) {
+    try {
+        const restaurants = await Restaurant.findAll()
+
+        return res.status(200).json({result : restaurants})
+    } catch (error) {
+        return res.status(500).json({error: 'Internal error server.'})
+    }
+}
+
+export async function getById (req, res) {
+    const { restaurantId } = req.params
+
+    try {
+        const restaurant = await Restaurant.findByPk(restaurantId)
+
+        if (!restaurant) return res.status(404).json({msg : 'Restaurant not found.'})
+
+        return res.status(200).json({result : restaurant})
+    } catch (error) {
+        return res.status(500).json({error: 'Internal error server.'})
+    }
+}
+
+export async function updateRestaurant (req, res) {
+    const { userId } = req
+    const { restaurantName } = req.params
+    const { name, address } = req.body
+    try {
+        if (!name || !address) return res.status(400).json({ error: 'Name and Address needed.' })
+
+        const restaurantData = await Restaurant.findOne({ where: {name: restaurantName}})
+    
+        if (!restaurantData) return res.status(404).json({error: 'Restaurant not found.'})
+        
+        if (restaurantData.userId != userId) return res.status(401).json({error: 'Unauthorized.'})
+        
+        await restaurantData.update({ name, address })
+
+        return res.status(200).json({msg: 'Restaurant has been updated.'})
+    } catch (error) {
+        return res.status(500).json({error: 'Internal Server Error.'})
+    }
+}
+
+export async function deleteRestaurant (req, res) {
+    const { userId } = req
+    const { restaurantId } = req.params
+
+    try {
+        const restaurantData = await Restaurant.findByPk(restaurantId)
+        
+        if (!restaurantData) return res.status(404).json({error: 'Restaurant not found.'})
+
+        if (restaurantData.userId != userId) return res.status(401).json({error: 'Unauthorized.'})
+
+        await Restaurant.destroy({
+            where: {
+                id: restaurantId
+            }
+        })
+
+        return res.status(200).json({msg: 'Restaurant has been deleted.'})
+    } catch (error) {
+        return res.status(500).json({error: 'Internal Server Error.'})
+    }
+}
