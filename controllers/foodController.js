3f6323cb6aafac0aa514d352db36405a25efb5ea
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-09-17 12:01:52 -0300

    controlador food

---
+++ controllers/foodController.js
@@ -0,0 +1,94 @@
+import Food from '../models/foodModel.js'
+import Restaurant from '../models/restaurantModel.js'
+
+export async function getAllByRestaurant (req, res) {
+    const { restaurantId } = req.params
+    const { userId } = req
+
+    try {
+        const dishes = await Food.findAll({
+            where: {restaurantId: restaurantId},
+            include: [{ //Para obtener los datos del restaurante en la consulta
+                model: Restaurant,
+            }]
+        })
+
+        if (!dishes) return res.status(400).json({error: 'Something Went Wrong.'})
+        console.log(dishes)
+        return res.status(200).json({data: dishes})
+    } catch (error) {
+        return res.status(500).json({error: 'Internal Server Error'})
+    }
+}
+
+export async function getFood (req, res) {
+    const { foodId } = req.params
+    const { userId } = req
+
+    try {
+        const foodData = await Food.findByPk(foodId)
+
+        if (!foodData) return res.status(404).json({error: 'Food not found.'})
+        
+        return res.status(200).json({data: foodData})
+    } catch (error) {
+        return res.status(500).json({error: 'Internal Server Error.'})
+    }
+}
+
+export async function deleteFood (req, res) {
+    const {foodId} = req.params
+    const {userId} = req
+
+    try {
+        const food = await Food.findOne({
+            where: {id: foodId},
+            include: [{ //Para obtener los datos del restaurante en la consulta
+                model: Restaurant,
+            }]
+        })
+
+        console.log(food, food.Restaurant.userId)
+
+        if (food.Restaurant.userId != userId) {
+            return res.status(401).json({msg: 'Unauthorized.'})
+        }
+
+        await Food.destroy({
+            where: {
+                id: foodId
+            }
+        })
+
+        return res.status(200).json({data: food, msg: 'Food has been deleted.'})
+    } catch (error) {
+        return res.status(500).json({error: 'Internal Server Error.'})
+    }
+}
+
+export async function updateFood (req, res) {
+    const {foodId} = req.params
+    const {userId} = req
+    const {name, price, description} = req.body
+
+    try {
+        const food = await Food.findOne({
+            where: {id: foodId},
+            include: [{ //Para obtener los datos del restaurante en la consulta
+                model: Restaurant,
+            }]
+        })
+
+        if (food.Restaurant.userId != userId) {
+            return res.status(401).json({msg: 'Unauthorized.'})
+        }
+
+        await food.update({name, price, description})
+        await food.reload()
+
+        return res.status(200).json({data: food, msg: 'Food has been updated.'})
+
+    } catch (error) {
+        return res.status(500).json({error: 'Internal Server Error.'})
+    }
+}
