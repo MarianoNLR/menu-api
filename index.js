a545c72cd77162e92bf715bf969722315dffc4de
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-09-18 17:50:30 -0300

    + use routers

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ import express from 'express'
 import 'dotenv/config'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import authRouter from './routes/authRouter.js'
+import foodRouter from './routes/foodRouter.js'
+import userRouter from './routes/userRouter.js'
+import restaurantRouter from './routes/restaurantRouter.js'
+
 const app = express()
 
 const PORT = process.env.PORT
@@ -12,6 +17,11 @@ app.use(cors({
     credentials: true
 }))
 
+//Routers
+app.use('/auth', authRouter)
+app.use('/users', userRouter)
+app.use('/restaurants', restaurantRouter)
+app.use('/foods', foodRouter)
 
 app.get('/', (req, res) => {
     res.send(`<h1>Hola Mundo</h1>`)
